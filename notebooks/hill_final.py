46fe9cda09ff5704b778256fae0758a56fde8ce9
Carlosrlpzi <carlosrlpzi@gmail.com>
2021-05-13 12:48:14 -0500

    route fixed #4

--- notebooks/hill_final.py
+++ notebooks/hill_final.py
@@ -6,6 +6,7 @@ import numpy as np
 def distance_matrix(coordinate):
     """
     calculate the distance among each suggest solution point
+
         input:
             coordinate[array]: array containig the points to be visited
         outputs:
@@ -13,17 +14,18 @@ def distance_matrix(coordinate):
     """
     matrix = []
     for i in range(len(coordinate)):
-        for j in range(len(coordinate)):
+        for j in range(len(coordinate)) :       
             p = np.linalg.norm(coordinate[i] - coordinate[j])
             matrix.append(p)
-    matrix = np.reshape(matrix, (len(coordinate), len(coordinate)))
-    # print(matrix)
+    matrix = np.reshape(matrix, (len(coordinate),len(coordinate)))
+    #print(matrix)
     return matrix
 
 
 def random_solution(matrix, initial_point):
     """
     create a random solution with the places to be visited
+
         input:
             matrix[array]: distance between points
             initial_point[integer]: number of the place to be visited first
@@ -33,22 +35,25 @@ def random_solution(matrix, initial_point):
     points = list(range(0, len(matrix)))
     solution = [initial_point]
     points.remove(initial_point)
-    for i in range(0, len(matrix) - 1):
+    for i in range(0, len(matrix)-1):
         random_point = points[random.randint(0, len(points) - 1)]
         solution.append(random_point)
         points.remove(random_point)
+    solution.append(solution[0])
 
     return solution
+
 
 
 def calculate_distance(matrix, solution):
     """
     returns the distance associated with a solution
+
         input:
             matrix[array]: distance between points
-            solution[list]: contains a propose random solution
+            solution[list]: contains a propose random solution 
         output:
-            distance[float]: distance cover a propose solution
+            distance[float]: distance cover a propose solution            
     """
     distance = 0
     for i in range(0, len(solution)):
@@ -59,7 +64,8 @@ def calculate_distance(matrix, solution):
 def neighbors(matrix, solution):
     """
     create neighbors of a propose solution
-        input:
+
+        input: 
             matrix[array]: distance between points
             solution[list]: contains a propose random solution
         output:
@@ -67,22 +73,23 @@ def neighbors(matrix, solution):
             best_path[float]: distance cover by the best_neighbor route
     """
     neighbors = []
-    for i in range(len(solution)):
-        for j in range(i + 1, len(solution)):
+    for i in range(1, len(solution)-1):
+        for j in range(i + 2, len(solution)-1):
             neighbor = solution.copy()
             neighbor[i] = solution[j]
             neighbor[j] = solution[i]
             neighbors.append(neighbor)
-
+            
     best_neighbor = neighbors[0]
     best_path = calculate_distance(matrix, best_neighbor)
-
+    
     for neighbor in neighbors:
         current_path = calculate_distance(matrix, neighbor)
         if current_path < best_path:
             best_path = current_path
             best_neighbor = neighbor
     return best_neighbor, best_path
+
 
 def best_solution(coordinate, initial_point = 0, tolerance = 1e-7, n_restarts = 100):
     """
@@ -123,5 +130,3 @@ def best_solution(coordinate, initial_point = 0, tolerance = 1e-7, n_restarts = 
         best_neighbor, best_neighbor_path = neighbors(matrix, neighbor)
         
     return global_path, global_solution, time.time() - start_time
-
-
